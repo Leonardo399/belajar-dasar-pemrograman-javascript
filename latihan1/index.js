0f18dbccda4d82972056f72cce7cbb45316d4f7a
Febrianto752 <febrianto.bekasi@gmail.com>
2021-09-08 14:14:46 +0700

    finished module 'Penanganan Error'

--- latihan1/index.js
+++ latihan1/index.js
@@ -1,5 +1,14 @@
-import {
-  coffeStock,
-  makeCoffe
-} from './state.js';
-console.log(coffeStock, makeCoffe);
+let myString = "";
+
+try {
+  myString += "a";
+  throw Error();
+} catch (e) {
+  myString += "b";
+} finally {
+  myString += "c";
+  throw Error();
+  myString += "d";
+}
+
+console.log(myString);
