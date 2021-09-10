caf1b602085551e3e85fd91440614fedec543a6a
Febrianto752 <febrianto.bekasi@gmail.com>
2021-09-10 16:36:57 +0700

    merapihkan git dari root folder

--- latihan2-npm/index.js
+++ latihan2-npm/index.js
@@ -1,7 +1,35 @@
-const _ = require('lodash');
+const makeCoffee = new Promise((resolve, reject) => {
+  resolve('ok ini menggunakan new');
+});
 
-const myarr = [1, 2, 3, 4];
+const makeCoffee2 = () => {
+  return new Promise((resolve, reject) => {
+    resolve('ini promise menggunakan function');
+  })
+}
 
-const sum = _.sum(myarr);
 
-console.log(sum);
+let kopiRobusta = makeCoffee.then((result) => {
+  console.log(result);
+});
+
+let kopiRobusta2 = makeCoffee2().then((result) => {
+  console.log(result);
+});
+
+// async function cobaPromise() {
+//   // const kopiHitam = await makeCoffee;
+//   const kopiHitam2 = await makeCoffee2();
+//   // console.log(kopiHitam);
+
+// }
+
+// cobaPromise();
+console.log('mulai');
+// let tmp = cobaPromise();
+console.log('selesai');
+
+// setTimeout(() => {
+//   console.log(tmp);
+
+// }, 100)
